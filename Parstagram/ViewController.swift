d72724de54a91a23fab91d7f243800704940f3ba
Stephen Baity <steve.baity@gmail.com>
2020-02-27 22:22:19 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Stephen Baity on 2/27/20.
+//  Copyright © 2020 Stephen Baity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
